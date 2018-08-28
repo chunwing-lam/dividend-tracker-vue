598a859e625f0330796b49a4f23224a083c8c1d9
chunwing-lam <chunwing.lam10@gmail.com>
2018-08-28 14:29:51 -0400

    style purchase row and add text placeholder

--- src/components/PurchasesRow.vue
+++ src/components/PurchasesRow.vue
@@ -2,22 +2,22 @@
   <div class="stock-row">
     <div class="stock action">
       <div class="icon">
-        <FaMinusSquareO />
+        <icon name="regular/minus-square" />
       </div>
       <div class="icon">
-        <FaLineChart />
+        <icon name="chart-line" />
       </div>
     </div>
-    <div class="stock symbol"></div>
-    <div class="stock share"></div>
-    <div class="stock market-price"></div>
-    <div class="stock market-value"></div>
-    <div class="stock entry-price"></div>
-    <div class="stock entry-value"></div>
-    <div class="stock weight"></div>
-    <div class="stock gain-loss"></div>
-    <div class="stock dividend-percentage"></div>
-    <div class="stock dividend"></div>
+    <div class="stock symbol">T</div>
+    <div class="stock share">65</div>
+    <div class="stock market-price">32.675</div>
+    <div class="stock market-value">2123.123</div>
+    <div class="stock entry-price">35.45</div>
+    <div class="stock entry-value">2304.25</div>
+    <div class="stock weight">7.5</div>
+    <div class="stock gain-loss red">-123</div>
+    <div class="stock dividend-percentage">6.12</div>
+    <div class="stock dividend">130.123123</div>
   </div>
 </template>
 
@@ -29,4 +29,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.icon {
+  cursor: pointer;
+}
+.stock-row:nth-child(odd) {
+  background: rgb(218, 222, 226);
+}
+.stock-row {
+  display: flex;
+}
+.stock.action {
+  width: 5%;
+  font-size: 1.3em;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-pack: space-evenly;
+  justify-content: space-evenly;
+}
+.stock {
+  width: 10%;
+  border: solid 1px white;
+  padding: 0.3em;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import '../node_modules/vue-awesome/icons/regular/minus-square'
+import '../node_modules/vue-awesome/icons/chart-line'
+import Icon from '../node_modules/vue-awesome/components/Icon'
 
 Vue.config.productionTip = false
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
+  components: {
+    App,
+    Icon
+  },
   template: '<App/>'
 })
